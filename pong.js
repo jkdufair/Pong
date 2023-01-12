e7b3ab1321f3ed45e14664441632cc1bc1ddc63a
Jason Dufair <jase@dufair.org>
2023-01-11 21:36:16 -0500

    Paddle moving!

--- pong.js
+++ pong.js
@@ -1,17 +1,80 @@
+var leftPaddleDirection = 'none';
+var rightPaddleDirection = 'none';
+var boardDiv;
+var boardRect;
+
+var gameStart = () => {
+	boardDiv = document.querySelector('#board');
+	boardRect = boardDiv.getBoundingClientRect();
+	moveBall();
+}
+
 var moveBall = () => {
-    var ball = document.querySelector('#ball');
-    var ballRect = ball.getBoundingClientRect();
+	// var ballDiv = document.querySelector('#ball');
+	// var ballRect = ballDiv.getBoundingClientRect();
 
-	var board = document.querySelector('#board');
-	var boardRect = board.getBoundingClientRect();
+	// if (ballRect.right < boardRect.right) {
+	// 	// move ball 10 to right
+	// 	ballDiv.style.left = ballRect.left + 10 + 'px';
+	// 	ballDiv.style.top = ballRect.top - 3 + 'px';
+	// }
 
-	if (ballRect.right < boardRect.right) {
-		// move ball 10 to right
-		ball.style.left = ballRect.left + 10 + "px";
-		ball.style.top = ballRect.top + 1 + "px";
+	if (leftPaddleDirection != 'none') {
+		var leftPaddleDiv = document.querySelector('#left-paddle')
+		var leftPaddleRect = leftPaddleDiv.getBoundingClientRect()
+		if (leftPaddleDirection == 'up') {
+			if (leftPaddleRect.top >= boardRect.top + 20) {
+				leftPaddleDiv.style.top = leftPaddleRect.top - 7 + 'px'
+			}
+		} else {
+			if (leftPaddleRect.bottom <= boardRect.bottom - 20) {
+				leftPaddleDiv.style.top = leftPaddleRect.top + 7 + 'px'
+			}
+		}
 	}
 
+	if (rightPaddleDirection != 'none') {
+		var rightPaddleDiv = document.querySelector('#right-paddle')
+		var rightPaddleRect = rightPaddleDiv.getBoundingClientRect()
+		if (rightPaddleDirection == 'up') {
+			if (rightPaddleRect.top >= boardRect.top + 20) {
+				rightPaddleDiv.style.top = rightPaddleRect.top - 7 + 'px'
+			}
+		} else {
+			if(rightPaddleRect.bottom <= boardRect.bottom - 20) {
+				rightPaddleDiv.style.top = rightPaddleRect.top + 7 + 'px'
+			}
+		}
+	}
 	requestAnimationFrame(() => { moveBall() })
 }
 
-window.addEventListener('load', moveBall)
+window.addEventListener('load', gameStart)
+
+document.addEventListener('keydown', e => {
+	if (e.key == 'q') {
+		leftPaddleDirection = 'up'
+	}
+
+	if (e.key == 'a') {
+		leftPaddleDirection = 'down'
+	}
+
+	if (e.key == 'o') {
+		rightPaddleDirection = 'up'
+	}
+
+	if (e.key == 'l') {
+		rightPaddleDirection = 'down'
+	}
+});
+
+document.addEventListener('keyup', e => {
+	if (e.key == 'q' || e.key == 'a') {
+		leftPaddleDirection = 'none'
+	}
+
+	if (e.key == 'o' || e.key == 'l') {
+		rightPaddleDirection = 'none'
+	}
+});
